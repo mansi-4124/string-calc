4c333082f84378638d92c2789f2b613e6bd2ef85
unknown <shintrems4@gmail.com>
2025-07-04 22:16:24 +0530

    Added functionality to add two numbers

--- src/add.js
+++ src/add.js
@@ -1,5 +1,11 @@
 function add(nums) {
   if (nums === "") return 0;
   if (nums.length == 1) return parseInt(nums);
+  let numArr = nums
+    .split(",")
+    .map((s) => s.trim())
+    .filter((s) => s !== "")
+    .map(Number);
+  return numArr.reduce((acc, n) => acc + n, 0);
 }
 module.exports = { add };
